b5b0656dcfbda1a0c5022fe52ef5b853126f9c36
nhatminh <pdn.minh1999@gmail.com>
2020-05-06 15:16:31 +0700

    init @angular/fire

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, Component } from "@angular/core";
+import { SimpleChanges } from "@angular/core";
+import { NgModule, Component, OnChanges } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 
 @Component({
-  selector: "app-root",
-  template: `<h1>Hello World 1</h1>`,
+  selector: "child-node",
+  template: ` <h1>Child Node</h1> `,
 })
-class AppComponent {}
+class AppChild implements OnChanges {
+  ngOnChanges(_changes: SimpleChanges): void {
+    console.log("Child Node is changed.");
+  }
+}
+
+@Component({
+  selector: "app-root",
+  template: `
+    <button (click)="switch()">Click me</button>
+    <h1 *ngIf="isTrue">State is true</h1>
+    <h1 *ngIf="!isTrue">State is false</h1>
+    <child-node></child-node>
+  `,
+})
+class AppComponent {
+  public isTrue = true;
+
+  public switch(): void {
+    this.isTrue = !this.isTrue;
+  }
+}
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, AppChild],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],

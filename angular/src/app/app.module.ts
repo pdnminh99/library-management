62af8dfa52d542f1ef573e2cb69f5bf236798180
nhatminh <pdn.minh1999@gmail.com>
2020-05-06 13:59:41 +0700

    remove uneccessary files

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, Component } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+
+@Component({
+  selector: "app-root",
+  template: `<h1>Hello World 1</h1>`,
+})
+class AppComponent {}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

2fdfad295fcdbd4b9597546d36c0bc2cf902775e
nhatminh <pdn.minh1999@gmail.com>
2020-05-11 16:32:54 +0700

    wrap login-card in a div

--- angular/src/app/pages/login.page.ts
+++ angular/src/app/pages/login.page.ts
@@ -2,7 +2,11 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "login-component",
-  template: ` <login-card-component></login-card-component> `,
+  template: `
+    <div style="display: table;">
+      <login-card-component style="display: table-cell;"></login-card-component>
+    </div>
+  `,
   styles: [``],
 })
 export class LoginComponent {}

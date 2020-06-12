713a35322ba3dc1c09950c5ca0a054daf9951d05
nhatminh <pdn.minh1999@gmail.com>
2020-06-12 08:57:41 +0700

    replace cloud functions with spring boot

---
+++ src/main/java/com/frontend/athene/AtheneApplication.java
@@ -0,0 +1,13 @@
+package com.frontend.athene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtheneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtheneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/frontend/athene/AtheneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frontend.athene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtheneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
